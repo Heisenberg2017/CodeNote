b71823916a9014c9f865120746612099e3752f94
Heisenberg2017 <289957126@qq.com>
2019-05-30 11:06:11 +0800

    3.8 feature cache_property

---
+++ 2019-5-30/cached_property.py
@@ -0,0 +1,54 @@
+# cache value to instance __dice__
+
+class cached_property(property):
+    """A decorator that converts a function into a lazy property.  The
+    function wrapped is called the first time to retrieve the result
+    and then that calculated result is used the next time you access
+    the value::
+        class Foo(object):
+            @cached_property
+            def foo(self):
+                # calculate something important here
+                return 42
+    The class has to have a `__dict__` in order for this property to
+    work.
+    """
+
+    # implementation detail: A subclass of python's builtin property
+    # decorator, we override __get__ to check for a cached value. If one
+    # chooses to invoke __get__ by hand the property will still work as
+    # expected because the lookup logic is replicated in __get__ for
+    # manual invocation.
+
+    def __init__(self, func, name=None, doc=None):
+        self.__name__ = name or func.__name__
+        self.__module__ = func.__module__
+        self.__doc__ = doc or func.__doc__
+        self.func = func
+
+    def __set__(self, obj, value):
+        obj.__dict__[self.__name__] = value
+
+    def __get__(self, obj, type=None):
+        if obj is None:
+            return self
+        value = obj.__dict__.get(self.__name__, "sign")
+        if value is "sign":
+            value = self.func(obj)
+            obj.__dict__[self.__name__] = value
+        return value
+
+
+class A:
+    @cached_property
+    def foo(self):
+        print("123")
+        return 1
+
+
+if __name__ == '__main__':
+    a = A()
+    b = A()
+    print(a.foo)
+    print(a.foo)
+    print(b.foo)
